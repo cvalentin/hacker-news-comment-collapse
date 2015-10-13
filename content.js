bed62dbdf28522e4b51163364a9c5f3a85775715
Carlo Valentin <me@carlovalentin.com>
2015-10-13 02:22:41 -0700

    Finish javascript, properly collapsing. :)

--- content.js
+++ content.js
@@ -1,27 +1,48 @@
-
-function toggle_comment(id){
-	console.log(id);
-}
-
+function init(){
 //Add collapse UI
-function add_collapse_UI(){
-	$("div span.comhead").append('<span class="com_collapse"><a href="#collapse" class="com_collapse">[+]</a></span>');
+	$("div span.comhead").append('<span class="com_collapse"><a href="#collapse" class="com_collapse">[-]</a></span>');
 
 	$("a.com_collapse").each(function (index) {
+		var item_id;
+		var id;
+
 		item_id = $(this).parent().prev().prev().prev().prev().attr("href");
 		id = item_id.substring(8,item_id.length);
 		$(this).attr("id", id);
 	});
 }
 
-
-function init(){
-	add_collapse_UI();
-}
-
 function main(){
 	$("a.com_collapse").click(function(){
+		//toggle UI
+		if ($(this).text() == "[+]"){
+			$(this).text("[-]");
+		}else{
+			$(this).text("[+]");
+		}
+		
+		//toggle comment
 		$(this).parent().parent().parent().next().next().toggle();
+
+		//toggle children
+		var comment_parent_level = $(this).parent().parent().parent().parent().parent().parent().parent().parent().parent().find("td.ind img").attr("width")/20; //width / 20
+		
+		var next_comment_container = $(this).parent().parent().parent().parent().parent().parent().parent().parent().parent().next();
+
+		var child_comment = true;
+
+		var breakout = 0;
+
+		while(child_comment){
+			next_comment_level = next_comment_container.find("td.ind img").attr("width")/20;
+			if (next_comment_level <= comment_parent_level){
+				child_comment = false;
+			}else{
+				next_comment_container.toggle();
+				next_comment_container = next_comment_container.next();
+			}
+		}
+		
 	});
 }
 
